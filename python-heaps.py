f04230a899ed90721d2ff98a84a3ea7c405e4d43
jordan dykstra <jtdykstra123@gmail.com>
2020-10-21 18:53:51 -0700

    add __lt__ for heap

--- python-heaps.py
+++ python-heaps.py
@@ -29,3 +29,30 @@ def smallestLargest():
     print(heapq.nlargest(3, l))
 print("smallest/largest")
 smallestLargest()
+
+import heapq
+
+class Solution:
+    class Point:
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+            
+        def __lt__(self, p):
+            return ((abs(self.x)**2 + abs(self.y)**2) > (abs(p.x)**2 + abs(p.y)**2))
+            
+        def getArrayVer(self):
+            return [self.x, self.y]
+    
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        h = []
+       
+        for p in points:
+            if (len(h) < K):
+                heapq.heappush(h, self.Point(p[0], p[1]))
+            else:
+                heapq.heappushpop(h, self.Point(p[0], p[1]))
+                
+        return [p.getArrayVer() for p in h]
+            
+        
